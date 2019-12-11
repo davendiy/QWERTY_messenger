6193e7e8ff7c04b028079fe1c6196ae29bc9065c
davendiy <davendiy@gmail.com>
2019-12-11 21:35:51 +0200

    regenerated the RSA keys

---
+++ regenerate_keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*-encoding: utf-8-*-
+
+# created: 11.12.2019
+# by David Zashkolny
+# 3 course, comp math
+# Taras Shevchenko National University of Kyiv
+# email: davendiy@gmail.com
+
+from src.sequrity import generate_signature_keys
+
+
+generate_signature_keys()
